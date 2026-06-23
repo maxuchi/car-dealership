26c021509558e3f63e888f5fc7bce0399b731610
maxuchi <malkhazi837@gmail.com>
2026-06-23 13:46:49 +0400

    Final project version

--- src/app/pages/cars/cars.ts
+++ src/app/pages/cars/cars.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Car } from '../../services/car';
 
 @Component({
   selector: 'app-cars',
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, CommonModule, FormsModule],
   templateUrl: './cars.html',
   styleUrl: './cars.css',
 })
@@ -15,13 +16,34 @@ export class Cars implements OnInit {
   selectedPrice = 'All';
 
   cars: any[] = [];
+  filteredCars: any[] = [];
 
   constructor(private carService: Car) {}
 
   ngOnInit() {
     this.carService.getCars().subscribe((data: any) => {
       this.cars = data;
-      console.log(this.cars);
+      this.filteredCars = data;
     });
   }
+
+  applyFilters() {
+
+    this.filteredCars = this.cars.filter(car => {
+
+      const brandMatch =
+        this.selectedBrand === 'All' ||
+        car.name.includes(this.selectedBrand);
+
+      const priceMatch =
+        this.selectedPrice === 'All' ||
+        (this.selectedPrice === 'Under' && car.price < 100000) ||
+        (this.selectedPrice === 'Over' && car.price >= 100000);
+
+      return brandMatch && priceMatch;
+
+    });
+
+  }
+
 }
